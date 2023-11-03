d981a990f0094fcb7553f56350706531a95cef2f
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-11-03 13:46:26 +0200

    Created a Models folder to hold future functional models initialization

---
+++ TelegramBotApp/Models/Currency.cs
@@ -0,0 +1,19 @@
+﻿namespace TelegramBot.App.Models;
+
+public class Currency
+{
+    public Currency(string nameCode, string emoji)
+    {
+        CurrencyNameCode = nameCode;
+        CurrencyEmoji = emoji;
+    }
+    
+    public string CurrencyNameCode { get; set; }
+
+    public string CurrencyEmoji { get; set; }
+    
+    public override string ToString()
+    {
+        return $"{CurrencyNameCode} {CurrencyEmoji}";
+    }
+}
---
+++ TelegramBotApp/Models/TBotConfiguration.cs
@@ -0,0 +1,8 @@
+﻿namespace TelegramBot.App.Models;
+
+public class TBotConfiguration
+{
+    public string TBotToken { get; set; } = string.Empty;
+    
+    public string CurrencyExchangePrivatBankApiKey { get; set; }
+}
