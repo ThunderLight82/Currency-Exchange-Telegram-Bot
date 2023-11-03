19215c6c891acac3dfe75649ca8f8159c4b988d8
Pecherky Oleg <yogidanfuzo@gmail.com>
2023-11-03 13:45:25 +0200

    Recreated "Telegram Bot" project as .Net Core Console App and write a test code for bot test lauch purpouse

---
+++ TelegramBotApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Configuration;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace TelegramBot.App;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        var config = new ConfigurationBuilder().
+            SetBasePath(AppContext.BaseDirectory).
+            AddJsonFile("appsetings.json").
+            Build();
+
+        var tBotToken = config.GetSection("BotConfiguration")["TBotToken"];
+        
+        var client = new TelegramBotClient(tBotToken);
+        client.StartReceiving(UpdateMessage, ErrorEvent);
+        
+        Console.WriteLine("Bot is started...");
+        
+        while (true)
+        {
+            await Task.Delay(1000);
+        }
+    }
+    
+    private static async Task UpdateMessage(ITelegramBotClient botClient, Update update, CancellationToken token)
+    {
+        var message = update.Message;
+
+        if (message.Text == "Test")
+        {
+            await botClient.SendTextMessageAsync(message.Chat.Id, "Пшел вон");
+        }
+    }
+    
+    private static async Task ErrorEvent (ITelegramBotClient tBotClient, Exception ex, CancellationToken token)
+    {
+        Console.WriteLine("An error occurred: " + ex.Message);
+    }
+}
