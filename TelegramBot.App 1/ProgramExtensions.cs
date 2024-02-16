24ad6784356fd7d460742a8e61661659d75e6ac5
Pecherky Oleg <yogidanfuzo@gmail.com>
2024-02-16 17:46:35 +0200

    Re-added App proj

---
+++ TelegramBot.App 1/ProgramExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TelegramBot.Services.Interfaces;
+
+namespace TelegramBot.App;
+
+// Background extension class with method that run loop to accept user messages  
+public class ProgramExtensions : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public ProgramExtensions(IServiceProvider serviceProvider)
+    {
+        _serviceProvider = serviceProvider;
+    }
+    
+    protected override async Task ExecuteAsync(CancellationToken token)
+    {
+        while (!token.IsCancellationRequested)
+        {
+            try
+            {
+                using var scope = _serviceProvider.CreateScope();
+                var receiver = scope.ServiceProvider.GetRequiredService<IReceiverService>();
+
+                await receiver.ReceiveAsync(token);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred in BackgroundService - ExecuteAsync: {ex}");
+                
+                await Task.Delay(TimeSpan.FromSeconds(3), token);
+            }
+        }
+    }
+}
